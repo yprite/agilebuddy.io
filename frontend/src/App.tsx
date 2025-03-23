dae0141e650e4f68ad8a54e51163e9c836189dd5
yprite <yprite@gmail.com>
2025-03-23 17:04:44 +0900

    fix: npm start 에러 해결
    Signed-off-by: yprite <yprite@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PlanningPoker from './pages/planning-poker';
 
 function App() {
   return (
-    <div className="App">
-      <h1>AgileBuddy</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">홈</Link>
+            </li>
+            <li>
+              <Link to="/planning-poker">스토리 포인트 산정</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<h1>AgileBuddy</h1>} />
+          <Route path="/planning-poker" element={<PlanningPoker />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/planning-poker/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlanningPoker: React.FC = () => {
+  return (
+    <div>
+      <h1>스토리 포인트 산정</h1>
+    </div>
+  );
+};
+
+export default PlanningPoker; 
